3222de4af718094c42721682d08ac0fab1b9e1e9
PedroSouza1111 <pedrohenriqueds0511@gmail.com>
2025-04-22 19:41:28 -0300

    iniciando projeto

---
+++ src/test/java/br/com/fiap/jogo_medieval_api/JogoMedievalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.jogo_medieval_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoMedievalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
